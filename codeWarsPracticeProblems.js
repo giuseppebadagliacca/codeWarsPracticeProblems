761978b1f74fbeb7328e96b50a8e8b168b9bc67b
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-05-31 16:19:38 -0400

    updated challanges

--- codeWarsPracticeProblems.js
+++ codeWarsPracticeProblems.js
@@ -174,3 +174,27 @@ let s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
 //We need a function that can transform a string into a number. What ways of achieving this do you know?
 
 const stringToNumber = str => Number(str)
+
+// return Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+//You need to round the answer to 2 decimal places and return it as String.
+
+//If the given value is 0 then it should return 0.00
+
+function SeriesSum(n){
+  if(n === 0){
+    return '0.00'
+  }else{
+    let numbersLeft = n
+    let sum = 1
+    let div = 4
+    while(numbersLeft>1){
+      sum += (1/div)
+      div += 3
+      numbersLeft--
+    }
+    return String(sum.toFixed(2))
+  }
+}
+
+console.log(SeriesSum(2))
