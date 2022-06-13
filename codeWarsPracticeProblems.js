1bcb6f50bedc4db862569473d03977e21a454cc5
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-06-13 08:59:44 -0400

    6/13/22

--- codeWarsPracticeProblems.js
+++ codeWarsPracticeProblems.js
@@ -202,4 +202,24 @@ function SeriesSum(n){
 
 const reverseWords = str => str.split('').reverse().join('');
 
-console.log(reverseWord('joey badagLIAcca'));
+// console.log(reverseWord('joey badagLIAcca'));
+
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+function getMiddle(s){
+  s = s.split('')
+  if(s.length%2 ===0){
+    while(s.length != 2){
+      s.pop()
+      s.shift()
+    }
+  }else{
+    while(s.length != 1){
+      s.pop()
+      s.shift()
+    }
+  }
+  return s.join('')
+}
+
+console.log(getMiddle('love'))
