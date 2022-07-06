73801bf5bbb8fe34fe3ebbe4e347fbadea7a9fd9
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-07-06 16:38:30 -0400

    7/6/22

--- codeWarsPracticeProblems.js
+++ codeWarsPracticeProblems.js
@@ -543,5 +543,18 @@ function duplicateEncode(word) {
   return finalString;
 }
 
-console.log(duplicateEncode("Success")) //")())())";
+//console.log(duplicateEncode("Success")) //")())())";
 
+//The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}. What if the string is empty? Then the result should be empty object literal, {}.
+function count(string) {
+  if (string.length === 0) {
+    return string;
+  }
+  const finObj = {};
+  for (let i = 0; i < string.length; i++) {
+    finObj[string[i]] ? finObj[string[i]] += 1 : finObj[string[i]] = 1;
+  }
+  return finObj;
+}
+
+console.log(count('Joejj'));
