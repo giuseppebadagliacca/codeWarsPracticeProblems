ebe81f339c3527d42fe3b939b792dc77048cf71a
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-07-13 11:59:01 -0400

    7/13/22

--- codeWarsPracticeProblems.js
+++ codeWarsPracticeProblems.js
@@ -696,7 +696,26 @@ function titleCase(str) {
 
 
 
+//Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result. Numbers can be from 1 to 9. So 1 will be the first word (not 0). If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
 
+function order(words) {
+  if (words === "") return "";
 
+  const splitWords = words.split(' ');
+  let count = 1;
+  let final = [];
 
+  while (splitWords.length >= count) {
+    const heta = splitWords.filter(x => {
+      if (x.includes(count)) {
+        return x;
+      }
+    })
+    final.push(heta);
+    count++;
 
+  }
+  return final.join(' ');
+}
+
+console.log(order("is2 Thi1s 3a T4est")); //"Thi1s is2 3a T4est"
