4bb13e37ef6f56c773b0eb62666b57c923e81aaa
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-07-22 09:13:33 -0400

    7/22/22

--- codeWarsPracticeProblems.js
+++ codeWarsPracticeProblems.js
@@ -903,3 +903,14 @@ function squareSum(numbers) {
   const squared = numbers.map(x => x * x);
   return squared.reduce((x, y) => x + y, 0);
 }
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
