8b605dd09812228ad5ce4b6b400169482f338c7a
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-07-24 12:28:05 -0400

    7/24/22

--- codeWarsPracticeProblems.js
+++ codeWarsPracticeProblems.js
@@ -914,3 +914,16 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+// JAVA!!! 
+// public class SumArray {
+
+//   public static double sum(double[] numbers) {
+//     double sum = 0;
+    
+//     for(int i=0;i<numbers.length;i++){
+//       sum += numbers[i];
+//     }
+//     return sum;
+//   }
+// }
